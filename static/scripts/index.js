8aa830f25a919174ef081204f5e4ae21352f0254
Jack Palmer <jleeto7@gmail.com>
2016-02-07 22:15:08 -0600

    remove interesting function ;(

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -4,9 +4,3 @@
   });
 })(jQuery);
 
-var hour = (new Date()).getHours();
-var night = hour > 19 || hour < 4;
-
-window.onload = function() {
-  document.body.classList.add(night ? 'night' : 'day');
-}
