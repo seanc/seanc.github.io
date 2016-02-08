f2379ed7588a92d066c3c30c74d2682a37c9c3b8
Jack Palmer <jleeto7@gmail.com>
2016-02-07 22:10:08 -0600

    add interesting function ;)

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -3,3 +3,10 @@
     $('.app').hide().fadeIn(800);
   });
 })(jQuery);
+
+var hour = (new Date()).getHours();
+var night = hour > 19 || hour < 4;
+
+window.onload = function() {
+  document.body.classList.add(night ? 'night' : 'day');
+}
